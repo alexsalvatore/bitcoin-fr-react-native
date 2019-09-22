35068fd56c40823aeb54c1feba49ab1c4da33542
Alex Krunch <alexandre@alexandersalvatore.local>
2019-09-22 18:24:29 +0200

    close to make it.

--- components/CustomWebView.js
+++ components/CustomWebView.js
@@ -7,6 +7,10 @@ export default class CustomWebView extends Component {
 
     webview = null;
     lastValideUrl = '';
+    backButton = '<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"'+
+    'width="16" height="16"'+
+    'viewBox="0 0 172 172"'+
+    'style=" fill:#000000;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#ffffff"><path d="M114.337,0l-82.087,80.50675l82.087,80.74325l14.663,-14.39425l-67.51,-66.1985l67.51,-66.263z"></path></g></g></svg>';
 
     constructor(props){
         super(props);
@@ -21,6 +25,19 @@ export default class CustomWebView extends Component {
     _onNavigationStateChange(webViewState){
 
         console.log(webViewState)
+
+        // Add a  back bottom
+        //const backButtonJS =
+       // "setTimeout(() => {"+
+        //"document.getElementById('td-top-mobile-toggle').style.display = 'none';"+
+       // "var leftBtn = document.getElementsByClassName('td-top-mobile-toggle')[0];"+
+        //"leftBtn.innerHTML = 'back';";
+        //"leftBtn.addEventListener('click', setTimeout(() =>{ window.history.back();}, 10) );"+
+        //"}, 10);";
+
+       // this.webview.injectJavaScript(backButtonJS);
+        //Add the button
+
         //is bitcoin.fr url?
         if (!webViewState.url.includes('bitcoin.fr')) {
 
@@ -32,11 +49,14 @@ export default class CustomWebView extends Component {
         } else if(webViewState.url === "https://bitcoin.fr/" && this.state.origin != "https://bitcoin.fr/") {
 
             const newURL =   this.state.origin ;
-            const redirectTo = 'window.location = "' + newURL + '"';
-            this.webview.injectJavaScript(redirectTo);
+            const redirectToJS = 'window.location = "' + newURL + '"';
+            this.webview.injectJavaScript(redirectToJS);
+        
 
         }else {
+
             this.lastValideUrl = webViewState.url;
+
         }
 
         this.setState({
@@ -46,13 +66,23 @@ export default class CustomWebView extends Component {
     }
 
     render (){
-        /*
-        var jsCode =  "document.querySelector('.fa-dot-circle-o').style.display = 'none';"
-        + "document.querySelector('.fa-bars').style.display = 'none';"
-        + "if(document.querySelector('.blog-post-social')) document.querySelector('.blog-post-social').style.display = 'none';"
-        + "if(document.querySelector('#disqus_thread')) document.querySelector('#disqus_thread').style.display = 'none';"
-        */
-       var jsCode = "";
+       
+        //td-header-menu-wrap 
+
+       const jsCode =  
+            "const targetNode = document.getElementsByClassName('home')[0];"+
+            "alert(targetNode);"+
+            "const config = { attributes: true, childList: true, subtree: true };"+
+            "const callback = function (mutationsList, observer) {"+
+                    "let leftBtn = document.getElementsByClassName('td-top-mobile-toggle');"+
+                    "alert(eftBtn.length);"+
+                    "if(leftBtn.length > 0){"+
+                    "leftBtn[0].display='none';"+
+                    "};"+
+            "};"+
+            "const observer = new MutationObserver(callback);";
+            "observer.observe(targetNode, config);";
+           
         return( <View style={style.view}>
 
                 <WebView
